3b13a035d1c003ffec39233f1844800a35344137
verschmelzen <ivan.komarov@protonmail.com>
2019-10-27 19:48:55 +0300

    Allow directory stat

--- data_node.py
+++ data_node.py
@@ -14,6 +14,14 @@ class DataNode:
         self._fs_root = fs_root.rstrip('/')
         self._workdir = '/'
 
+    def _path_to_fs(self, path: str) -> str:
+        if path.startswith('/'):
+            return path_join(self._fs_root, path)
+        return path_join(self._fs_root, self._workdir, path)
+
+    def _fs_to_path(self, fs_path):
+        return fs_path[len(self._fs_root):] or '/'
+
     def mkfs(self):
         if os.path.exists(self._fs_root):
             shutil.rmtree(self._fs_root)
@@ -29,14 +37,6 @@ class DataNode:
             total, used, free size in bytes on disk
         """
         return shutil.disk_usage(self._fs_root)
-
-    def _path_to_fs(self, path: str) -> str:
-        if path.startswith('/'):
-            return path_join(self._fs_root, path)
-        return path_join(self._fs_root, self._workdir, path)
-
-    def _fs_to_path(self, fs_path):
-        return fs_path[len(self._fs_root):] or '/'
 
     def cd(self, path: str):
         fs_path = self._path_to_fs(path)
@@ -98,8 +98,6 @@ class DataNode:
         fs_path = self._path_to_fs(path)
         if not os.path.exists(fs_path):
             raise Exception(f'{path} does not exist')
-        if not os.path.isfile(fs_path):
-            raise Exception(f'{path} is not a file')
         return self._fs_to_path(fs_path), os.stat(fs_path).st_size
 
     def cp(self, src: str, dst: str):
@@ -119,3 +117,4 @@ class DataNode:
         if os.path.exists(abs_dst):
             raise Exception(f'{dst} already exists')
         shutil.move(abs_src, abs_dst)
+
