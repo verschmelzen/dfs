abb77f710603fca7c3f43ab56b2207606b20df5c
verschmelzen <ivan.komarov@protonmail.com>
2019-10-27 19:29:00 +0300

    Fix path resolving

--- data_node.py
+++ data_node.py
@@ -2,16 +2,22 @@ import os
 import shutil
 
 
+def path_join(first, *args):
+    return os.path.normpath(
+        os.path.join(first, *(a.strip('/') for a in args))
+    )
+
+
 class DataNode:
 
-    def __init__(self, root: str):
-        self.root = root
-        self.workdir = root
+    def __init__(self, fs_root: str):
+        self._fs_root = fs_root.rstrip('/')
+        self._workdir = '/'
 
     def mkfs(self):
-        if os.path.exists(self.root):
-            shutil.rmtree(self.root)
-        os.makedirs(self.root)
+        if os.path.exists(self._fs_root):
+            shutil.rmtree(self._fs_root)
+        os.makedirs(self._fs_root)
 
     def df(self) -> tuple:
         """
@@ -22,81 +28,83 @@ class DataNode:
         tuple(int, int, int)
             total, used, free size in bytes on disk
         """
-        return shutil.disk_usage(self.root)
+        return shutil.disk_usage(self._fs_root)
 
-    def _resolve_path(self, path: str) -> str:
+    def _path_to_fs(self, path: str) -> str:
         if path.startswith('/'):
-            return self.root + path
-        return os.path.join(self.workdir, path)
+            return path_join(self._fs_root, path)
+        return path_join(self._fs_root, self._workdir, path)
 
-    def _dfs_path(self, abs_path):
-        return abs_path[len(self.root) + 1:]
+    def _fs_to_path(self, fs_path):
+        return fs_path[len(self._fs_root):] or '/'
 
     def cd(self, path: str):
-        abs_path = self._resolve_path(path)
-        if not os.path.isdir(abs_path):
+        fs_path = self._path_to_fs(path)
+        if not os.path.isdir(fs_path):
             raise Exception(f'{path} is not a dir')
-        self.workdir = abs_path
+        self._workdir = self._fs_to_path(fs_path)
 
-    def ls(self, path: str) -> list:
-        abs_path = self._resolve_path(path)
-        if not os.path.isdir(abs_path):
+    def ls(self, path: str = None) -> list:
+        if path == None:
+            path = self._workdir
+        fs_path = self._path_to_fs(path)
+        if not os.path.isdir(fs_path):
             raise Exception(f'{path} is not a dir')
-        return os.listdir(abs_path)
+        return os.listdir(fs_path)
 
     def mkdir(self, path: str):
-        abs_path = self._resolve_path(path)
-        if os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if os.path.exists(fs_path):
             raise Exception(f'{path} already exists')
-        os.makedirs(abs_path)
+        os.makedirs(fs_path)
 
     def rmdir(self, path: str, force=False):
-        abs_path = self._resolve_path(path)
-        if not os.path.isdir(abs_path):
+        fs_path = self._path_to_fs(path)
+        if not os.path.isdir(fs_path):
             raise Exception(f'{path} is not a dir')
         if len(os.listdir(path)) > 0 and not force:
             raise Exception(f'{path} is not empty')
-        shutil.rmtree(abs_path)
+        shutil.rmtree(fs_path)
 
     def touch(self, path: str):
-        abs_path = self._resolve_path(path)
-        if os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if os.path.exists(fs_path):
             raise Exception(f'{path} already exists')
-        open(abs_path, 'a').close()
+        open(fs_path, 'a').close()
 
     def cat(self, path: str) -> bytes:
-        abs_path = self._resolve_path(path)
-        if not os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if not os.path.exists(fs_path):
             raise Exception(f'{path} does not exist')
-        with open(abs_path, 'rb') as file:
+        with open(fs_path, 'rb') as file:
             return file.read()
 
     def tee(self, path: str, data: bytes):
-        abs_path = self._resolve_path(path)
-        if os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if os.path.exists(fs_path):
             raise Exception(f'{path} already exist')
-        with open(abs_path, 'wb') as file:
+        with open(fs_path, 'wb') as file:
             file.write(data)
 
     def rm(self, path: str):
-        abs_path = self._resolve_path(path)
-        if not os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if not os.path.exists(fs_path):
             raise Exception(f'{path} does not exist')
-        if os.path.isdir(abs_path):
+        if os.path.isdir(fs_path):
             raise Exception(f'{path} is a directory')
-        os.remove(abs_path)
+        os.remove(fs_path)
 
     def stat(self, path: str) -> tuple:
-        abs_path = self._resolve_path(path)
-        if not os.path.exists(abs_path):
+        fs_path = self._path_to_fs(path)
+        if not os.path.exists(fs_path):
             raise Exception(f'{path} does not exist')
-        if not os.path.isfile(abs_path):
+        if not os.path.isfile(fs_path):
             raise Exception(f'{path} is not a file')
-        return self._dfs_path(abs_path), os.stat(abs_path).st_size
+        return self._fs_to_path(fs_path), os.stat(fs_path).st_size
 
     def cp(self, src: str, dst: str):
-        abs_src = self._resolve_path(src)
-        abs_dst = self._resolve_path(dst)
+        abs_src = self._path_to_fs(src)
+        abs_dst = self._path_to_fs(dst)
         if not os.path.exists(abs_src):
             raise Exception(f'{src} does not exist')
         if os.path.exists(abs_dst):
@@ -104,8 +112,8 @@ class DataNode:
         shutil.copy(abs_src, abs_dst)
 
     def mv(self, src: str, dst: str):
-        abs_src = self._resolve_path(src)
-        abs_dst = self._resolve_path(dst)
+        abs_src = self._path_to_fs(src)
+        abs_dst = self._path_to_fs(dst)
         if not os.path.exists(abs_src):
             raise Exception(f'{src} does not exist')
         if os.path.exists(abs_dst):
