ab03cf09f080e66a52d06baf42dee4cc5e874511
verschmelzen <ivan.komarov@protonmail.com>
2019-10-27 23:53:16 +0300

    Implement serialization and user error reporting

--- data_node.py
+++ data_node.py
@@ -1,11 +1,50 @@
 import os
 import shutil
 
+from util import CommandError
+
 
 def path_join(first, *args):
     return os.path.normpath(
         os.path.join(first, *(a.strip('/') for a in args))
     )
+
+def deserialize(stream):
+    has_blob = False
+    path = b''
+    b = stream.read(1)
+    while b:
+        if b == b' ':
+            break
+        if b == b'\0':
+            has_blob = True
+            break
+        path += path + b
+        b = stream.read(1)
+    path = path.decode('utf-8')
+    if not b:
+        return (path or None,)
+
+    if has_blob:
+        return path, stream.read()
+
+    b = stream.read(1)
+    if b == b'!':
+        nb = stream.read(1)
+        if not nb:
+            return path, True
+        b += nb
+    b += stream.read()
+    return path, b.decode('utf-8')
+
+def serialize(data):
+    if type(data) == bytes:
+        return data
+    try:
+        iterator = iter(data)
+    except TypeError:
+        return str(data).encode('utf-8')
+    return ' '.join(str(x) for x in iterator).encode('utf-8')
 
 
 class DataNode:
@@ -41,7 +80,7 @@ class DataNode:
     def cd(self, path: str):
         fs_path = self._path_to_fs(path)
         if not os.path.isdir(fs_path):
-            raise Exception(f'{path} is not a dir')
+            raise CommandError(f'{path} is not a dir')
         self._workdir = self._fs_to_path(fs_path)
 
     def ls(self, path: str = None) -> list:
@@ -49,114 +88,86 @@ class DataNode:
             path = self._workdir
         fs_path = self._path_to_fs(path)
         if not os.path.isdir(fs_path):
-            raise Exception(f'{path} is not a dir')
+            raise CommandError(f'{path} is not a dir')
         return os.listdir(fs_path)
 
     def mkdir(self, path: str):
         fs_path = self._path_to_fs(path)
         if os.path.exists(fs_path):
-            raise Exception(f'{path} already exists')
+            raise CommandError(f'{path} already exists')
         os.makedirs(fs_path)
 
     def rmdir(self, path: str, force=False):
         fs_path = self._path_to_fs(path)
         if not os.path.isdir(fs_path):
-            raise Exception(f'{path} is not a dir')
+            raise CommandError(f'{path} is not a dir')
         if len(os.listdir(path)) > 0 and not force:
-            raise Exception(f'{path} is not empty')
+            raise CommandError(f'{path} is not empty')
         shutil.rmtree(fs_path)
 
     def touch(self, path: str):
         fs_path = self._path_to_fs(path)
         if os.path.exists(fs_path):
-            raise Exception(f'{path} already exists')
+            raise CommandError(f'{path} already exists')
         open(fs_path, 'a').close()
 
     def cat(self, path: str) -> bytes:
         fs_path = self._path_to_fs(path)
         if not os.path.exists(fs_path):
-            raise Exception(f'{path} does not exist')
+            raise CommandError(f'{path} does not exist')
         with open(fs_path, 'rb') as file:
             return file.read()
 
     def tee(self, path: str, data: bytes):
         fs_path = self._path_to_fs(path)
         if os.path.exists(fs_path):
-            raise Exception(f'{path} already exist')
+            raise CommandError(f'{path} already exist')
         with open(fs_path, 'wb') as file:
             file.write(data)
 
     def rm(self, path: str):
         fs_path = self._path_to_fs(path)
         if not os.path.exists(fs_path):
-            raise Exception(f'{path} does not exist')
+            raise CommandError(f'{path} does not exist')
         if os.path.isdir(fs_path):
-            raise Exception(f'{path} is a directory')
+            raise CommandError(f'{path} is a directory')
         os.remove(fs_path)
 
     def stat(self, path: str) -> tuple:
         fs_path = self._path_to_fs(path)
         if not os.path.exists(fs_path):
-            raise Exception(f'{path} does not exist')
+            raise CommandError(f'{path} does not exist')
         return self._fs_to_path(fs_path), os.stat(fs_path).st_size
 
     def cp(self, src: str, dst: str):
         abs_src = self._path_to_fs(src)
         abs_dst = self._path_to_fs(dst)
         if not os.path.exists(abs_src):
-            raise Exception(f'{src} does not exist')
+            raise CommandError(f'{src} does not exist')
         if os.path.exists(abs_dst):
-            raise Exception(f'{dst} already exists')
+            raise CommandError(f'{dst} already exists')
         shutil.copy(abs_src, abs_dst)
 
     def mv(self, src: str, dst: str):
         abs_src = self._path_to_fs(src)
         abs_dst = self._path_to_fs(dst)
         if not os.path.exists(abs_src):
-            raise Exception(f'{src} does not exist')
+            raise CommandError(f'{src} does not exist')
         if os.path.exists(abs_dst):
-            raise Exception(f'{dst} already exists')
+            raise CommandError(f'{dst} already exists')
         shutil.move(abs_src, abs_dst)
 
-    def _deserialize(self, stream):
-        has_blob = False
-        path = b''
-        b = stream.read(1)
-        while b:
-            if b == b' ':
-                break
-            if b == b'\0':
-                has_blob = True
-                break
-            path += path + b
-            b = stream.read(1)
-        path = path.decode('utf-8')
-        if not b:
-            return path
-
-        if has_blob:
-            return path, stream.read()
-
-        b = stream.read(1)
-        if b == b'!':
-            nb = stream.read(1)
-            if not nb:
-                return path, True
-            b += nb
-        b += stream.read()
-        return path, b.decode('utf-8')
-
-    URLS = {
-        '/mkfs': (mkfs, _deserialize),
-        '/cd': (cd, _deserialize),
-        '/ls': (ls, _deserialize),
-        '/mkdir': (mkdir, _deserialize),
-        '/rmdir': (rmdir, _deserialize),
-        '/touch': (touch, _deserialize),
-        '/cat': (cat, _deserialize),
-        '/rm': (rm, _deserialize),
-        '/stat': (stat, _deserialize),
-        '/cp': (cp, _deserialize),
-        '/mv': (mv, _deserialize),
+    PATHS = {
+        '/mkfs': (mkfs, deserialize, serialize),
+        '/cd': (cd, deserialize, serialize),
+        '/ls': (ls, deserialize, serialize),
+        '/mkdir': (mkdir, deserialize, serialize),
+        '/rmdir': (rmdir, deserialize, serialize),
+        '/touch': (touch, deserialize, serialize),
+        '/cat': (cat, deserialize, serialize),
+        '/rm': (rm, deserialize, serialize),
+        '/stat': (stat, deserialize, serialize),
+        '/cp': (cp, deserialize, serialize),
+        '/mv': (mv, deserialize, serialize),
     }
 
--- server.py
+++ server.py
@@ -2,7 +2,10 @@ import os
 import sys
 from importlib import import_module
 from wsgiref.simple_server import make_server
-from wsgiref.util import application_uri
+from wsgiref.util import request_uri
+from urllib.parse import urlparse
+
+from util import CommandError
 
 
 def import_class(path):
@@ -13,12 +16,21 @@ def import_class(path):
     return getattr(sys.modules[__name__], klass)
 
 
-def route_request(env, start_request):
+def route_request(env, start_response):
     node = env['DFS_NODE_CLASS']
-    url = application_uri(env).rstrip('/')
-    method, deserialize = node.URLS[url]
-    method(*deserialize(env['wsgi.input']))
-    # TODO: write response
+    uri = request_uri(env).rstrip('/')
+    path = urlparse(uri).path
+    command, deserialize, serialize = node.PATHS[path]
+    args = deserialize(env['wsgi.input'])
+    try:
+        resp = command(node, *args)
+    except CommandError as e:
+        start_response(
+            '400 Bad Request',
+            [('Content-type', 'text/plain')],
+        )
+        return [str(e).enode('utf-8')]
+    return [serialize(resp)]
 
 
 if __name__ == '__main__':
@@ -26,13 +38,13 @@ if __name__ == '__main__':
         os.environ['DFS_NODE_CLASS']
     )(os.environ['DFS_FS_ROOT'])
 
-    def wsgi_app(env, start_request):
+    def wsgi_app(env, start_response):
         env['DFS_NODE_CLASS'] = node
-        return route_request(env, start_request)
+        return route_request(env, start_response)
 
     with make_server(
-        os.environ['DFS_HOST'],
-        int(os.environ['DFS_PORT']),
+        os.environ.get('DFS_HOST', '0.0.0.0'),
+        int(os.environ.get('DFS_PORT', '8180')),
         wsgi_app,
     ) as server:
         server.serve_forever()
